1f044ada1e958640287bd553223696bfbbef2162
Shaun Mullen <smmullen90@gmail.com>
2020-05-30 16:36:08 -0700

    Update countdown styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { inject, observer } from 'mobx-react';
 
+function formatNumber(number) {
+  return number.toString().padStart(2, '0');
+}
 
 const App = inject('countdownStore')(observer((props) => {
   const { countdownStore } = props;
@@ -19,15 +22,34 @@ const App = inject('countdownStore')(observer((props) => {
     countdownDate
   } = countdownStore;
 
+  if (isNaN(daysUntil)) {
+    return;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <p>Shaun heads to PDX in</p>
-          <p className="countdown-date">
-            {daysUntil} days, {hoursUntil} hours, {minutesUntil} minutes, {secondsUntil} seconds
-          </p>
-          <p>on {countdownDate.toLocaleDateString()}.</p>
+        <div className="content">
+          <p className="support">Shaun heads to PDX in</p>
+          <div className="countdown">
+            <div>
+              <p className="countdown__number">{formatNumber(daysUntil)}</p>
+              <p className="countdown__title">days</p>
+            </div>
+            <div>
+              <p className="countdown__number">{formatNumber(hoursUntil)}</p>
+              <p className="countdown__title">hrs</p>
+            </div>
+            <div>
+              <p className="countdown__number">{formatNumber(minutesUntil)}</p>
+              <p className="countdown__title">mins</p>
+            </div>
+            <div>
+              <p className="countdown__number">{formatNumber(secondsUntil)}</p>
+              <p className="countdown__title">secs</p>
+            </div>
+          </div>
+          <p className="support">on {countdownDate.toLocaleDateString()}.</p>
         </div>
       </header>
     </div>
